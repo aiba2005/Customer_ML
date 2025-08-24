f2b0efb8b9ba4b764964a335f374fe87c9ded9f9
aiba2005 <sharshenbekovaitbek@gmail.com>
2025-08-24 13:48:22 +0600

    finish

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ customer_app = FastAPI()
 scaler = joblib.load('scaler_2.pkl')
 model_lg = joblib.load('model_lg (1).pkl')
 model_svm = joblib.load('model_svm.pkl')
-
+model_tree = joblib.load('model_tree1.pkl')
 
 contract = ['Month-to-month', 'One year', 'Two year']
 internetService = ['DSL', 'Fiber optic', 'No']
@@ -32,6 +32,38 @@ class CustomerChema(BaseModel):
     InternetService: str
     OnlineSecurity: str
     TechSupport: str
+
+
+
+@customer_app.post('/predict/tree/')
+async def predict(cust: CustomerChema):
+    cust_dict = cust.dict()
+
+    Contract_1 = cust_dict.pop('Contract')
+    Contract0_1 = [ 1 if Contract_1 == i else 0 for i in contract]
+
+
+
+    InternetService1 = cust_dict.pop('InternetService')
+    InternetService0_1 = [1 if InternetService1 == i else 0 for i in internetService]
+
+
+    OnlineSecurity1 = cust_dict.pop('OnlineSecurity')
+    OnlineSecurity0_1 = [1 if OnlineSecurity1 == i else 0 for i in onlineSecurity]
+
+
+    TechSupport1 = cust_dict.pop('TechSupport')
+    TechSupport0_1 = [1 if TechSupport1 == i else 0 for i in techSupport]
+
+
+    features = list(cust_dict.values()) + Contract0_1 + InternetService0_1 + OnlineSecurity0_1 + TechSupport0_1
+
+    scaled_data = scaler.transform([features])
+    pred = model_tree.predict(scaled_data)[0]
+    return {"prediction": "Yes" if pred == 1 else "No"}
+
+
+
 
 
 
@@ -62,6 +94,7 @@ async def predict(cust: CustomerChema):
     scaled_data = scaler.transform([features])
     pred = model_lg.predict(scaled_data)[0]
     return {"prediction": "Yes" if pred == 1 else "No"}
+
 
 
 
